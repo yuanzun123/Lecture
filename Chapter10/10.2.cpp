9e88f73100d20d8c0a7af5ea10817a08470bfb2b
yuanzun123 <85395187+yuanzun123@users.noreply.github.com>
2022-03-13 16:46:13 +0800

    Update 10.2.cpp

--- Chapter10/10.2.cpp
+++ Chapter10/10.2.cpp
@@ -51,4 +51,4 @@ int main() {
 }
 //1.substr的用法：https://blog.csdn.net/qq_42185999/article/details/106985376?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522164715872616781685375791%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=164715872616781685375791&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_click~default-2-106985376.article_score_rank&utm_term=substr%E5%87%BD%E6%95%B0%E7%94%A8%E6%B3%95&spm=1018.2226.3001.4187
 //2.str.find(char c)函数：返回str中第一个c字符的索引
-//3.由前序和中序构建二叉树的思路：前序第一个为二叉树的根节点，之后可由此构建递推，将一个二叉树划分为两个小的二叉树
+//3.由前序和中序构建二叉树的思路：前序第一个为二叉树的根节点，之后可由此构建递推，将一个二叉树划分为两个小的二叉树，构建节点的终结是如果传入长度为0，返回NULL
