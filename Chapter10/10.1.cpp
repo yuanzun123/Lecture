ea567eb240e61403eb263c4a8dd8558f338cb8cc
yuanzun123 <85395187+yuanzun123@users.noreply.github.com>
2022-03-13 15:18:05 +0800

    Update 10.1.cpp

--- Chapter10/10.1.cpp
+++ Chapter10/10.1.cpp
@@ -49,3 +49,73 @@ int main() {
     }
     return 0;
 }
+//new语法（https://www.runoob.com/cplusplus/cpp-dynamic-memory.html）
+//函数中定义new等同于static变量，这样就可以从函数中返回在函数中定义的指针（https://www.runoob.com/cplusplus/cpp-return-pointer-from-functions.html）（https://blog.csdn.net/weixin_41232202/article/details/96452228?spm=1001.2101.3001.6650.4&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4.pc_relevant_default&utm_relevant_index=8）
+#include <iostream>
+//using namespace std;
+//
+//int* example1()
+//{
+//    //可以在new后面直接赋值
+//    int* p = new int(3);
+//    //也可以单独赋值
+//    //*p = 3;
+//
+//    //如果不想使用指针，可以定义一个变量，在new之前用“*”表示new出来的内容
+//    //int q = *new int;
+//    //q = 1;
+//    //cout << q << endl;
+//
+//    return p;
+//}
+//
+//int* example2()
+//{
+//    //当new一个数组时，同样用一个指针接住数组的首地址
+//    int* q = new int[3];
+//    for (int i = 0; i < 3; i++)
+//        q[i] = i;
+//
+//    return q;
+//}
+//
+//struct student
+//{
+//    string name;
+//    int score;
+//};
+//
+//
+//student* example3()
+//{
+//    //这里是用一个结构体指针接住结构体数组的首地址
+//    //对于结构体指针，个人认为目前这种赋值方法比较方便
+//    student* stlist = new student[3]{ {"abc", 90}, {"bac", 78}, {"ccd", 93} };
+//
+//    return stlist;
+//}
+//
+//
+//
+//int main()
+//{
+//    int* e1 = example1();
+//    cout << "e1: " << *e1 << endl;
+//    delete e1;
+//    int* e2 = example2();
+//    for (int i = 0; i < 3; i++)
+//        cout << e2[i] << " ";
+//    cout << endl;
+//    delete[]e2;
+//
+//    student* st1 = example3();
+//
+//    for (int i = 0; i < 3; i++)
+//        cout << st1[i].name << " " << st1[i].score << endl;
+//
+//    delete[]st1;
+//
+//    return 0;
+//}
+//以上为示例从函数中返回new定义的指针，并且不用delete
+//
